9bcc30b3b4cfe926c814f1711404c23b6481a12a
fancylaputa <100014614+fancylaputa@users.noreply.github.com>
2022-04-23 13:33:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,8 +14,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git leanpackages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
 echo 'src-git leanluci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
