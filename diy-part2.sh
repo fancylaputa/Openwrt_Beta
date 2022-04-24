837563e110d32330c2ecade8a9d7087765adf0a0
fancylaputa <100014614+fancylaputa@users.noreply.github.com>
2022-04-24 13:01:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -58,3 +58,5 @@ mkdir -p package/helloworld
 for i in "dns2socks" "microsocks" "ipt2socks" "pdnsd-alt" "redsocks2"; do \
   svn checkout "https://github.com/immortalwrt/packages/trunk/net/$i" "package/helloworld/$i"; \
 done
+
+svn checkout https://github.com/coolsnowwolf/lede/trunk/package/lean package/lean
